a2ff7e4ff7e1a73e0a6c57194a59a613bf7cdbf6
nnb2rp <nnb2rp@gmail.com>
2024-01-28 08:00:50 -0300

    Novo Layout

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let year = document.querySelector("#year");
+year.textContent = new Date().getFullYear();
+
+let hour = new Date().getHours();
+
+if (hour >= 6 && hour <= 18) {
+  document.body.style.backgroundImage =
+    "url('./assets/imagens/background_day.jpg')";
+}
