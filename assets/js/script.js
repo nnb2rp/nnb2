3fa1a848004ee7370940bc4cdeaf44e35d254c6a
nnb2rp <nnb2rp@gmail.com>
2024-01-30 06:26:12 -0300

    Ajustes de Layout e Lógica

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ year.textContent = new Date().getFullYear();
 
 let hour = new Date().getHours();
 
-if (hour >= 6 && hour <= 18) {
+if (hour >= 6 && hour < 18) {
   document.body.style.backgroundImage =
     "url('./assets/imagens/background_day.jpg')";
 }
